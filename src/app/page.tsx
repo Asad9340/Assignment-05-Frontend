8af0262b9a59bb16669b6a57969c027397c8660e
Asad9340 <asadulimran1999@gmail.com>
2026-03-14 23:31:43 +0600

    added project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h3>hello world</h3>
+      <Button className="btn btn-primary">Click Me</Button>
+  </div>
+  );
+} 
