2df2da33c88e646b1ece725d885e3198188b1d76
Asad9340 <asadulimran1999@gmail.com>
2026-03-29 00:29:43 +0600

    auth utils and submit and input field design

--- src/lib/authUtils.ts
+++ src/lib/authUtils.ts
@@ -1,100 +1,87 @@
-export type UserRole = "SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT";
+export type UserRole = 'ADMIN' | 'USER' | 'DOCTOR';
 
-export const authRoutes = [ "/login", "/register", "/forgot-password", "/reset-password", "/verify-email" ];
+export const authRoutes = [
+  '/login',
+  '/register',
+  '/forgot-password',
+  '/reset-password',
+  '/verify-email',
+];
 
-export const isAuthRoute = (pathname : string) => {
-    return authRoutes.some((router : string) => router === pathname);
-}
-
-export type RouteConfig = {
-    exact : string[],
-    pattern : RegExp[]
-}
-
-export const commonProtectedRoutes : RouteConfig = {
-    exact : ["/my-profile", "/change-password"],
-    pattern : []
-}
-
-export const doctorProtectedRoutes : RouteConfig = {
-    pattern: [/^\/doctor\/dashboard/ ], // Matches any path that starts with /doctor/dashboard
-    exact : []
-}
-
-export const adminProtectedRoutes : RouteConfig = {
-    pattern: [/^\/admin\/dashboard/ ], // Matches any path that starts with /admin/dashboard
-    exact : []
-}
-
-// export const superAdminProtectedRoutes : RouteConfig = {
-//     pattern: [/^\/admin\/dashboard/ ], // Matches any path that starts with /super-admin/dashboard
-//     exact : []
-// }
-
-export const patientProtectedRoutes : RouteConfig = {
-    pattern: [/^\/dashboard/ ], // Matches any path that starts with /dashboard
-    exact : [ "/payment/success"]
+export const isAuthRoute = (pathname: string) => {
+  return authRoutes.some((router: string) => router === pathname);
 };
 
-export const isRouteMatches = (pathname : string, routes : RouteConfig) => {
-    if(routes.exact.includes(pathname)) {
-        return true;
-    }
-    return routes.pattern.some((pattern : RegExp) => pattern.test(pathname));
-}
+export type RouteConfig = {
+  exact: string[];
+  pattern: RegExp[];
+};
 
-export const getRouteOwner = (pathname : string) : "SUPER_ADMIN" | "ADMIN" | "DOCTOR" | "PATIENT" | "COMMON" | null => {
-    if(isRouteMatches(pathname, doctorProtectedRoutes)) {
-        return "DOCTOR";
-    }
+export const commonProtectedRoutes: RouteConfig = {
+  exact: ['/my-profile', '/change-password'],
+  pattern: [],
+};
 
-    // if (isRouteMatches(pathname, superAdminProtectedRoutes)) {
-    //     return "SUPER_ADMIN";
-    // }
+export const adminProtectedRoutes: RouteConfig = {
+  pattern: [/^\/admin\/dashboard/],
+  exact: [],
+};
 
-    if(isRouteMatches(pathname, adminProtectedRoutes)) {
-        return "ADMIN";
-    }
+export const userProtectedRoutes: RouteConfig = {
+  pattern: [/^\/dashboard/],
+  exact: [],
+};
 
-    if(isRouteMatches(pathname, patientProtectedRoutes)) {
-        return "PATIENT";
-    }
+export const isRouteMatches = (pathname: string, routes: RouteConfig) => {
+  if (routes.exact.includes(pathname)) {
+    return true;
+  }
+  return routes.pattern.some((pattern: RegExp) => pattern.test(pathname));
+};
 
-    if(isRouteMatches(pathname, commonProtectedRoutes)) {
-        return "COMMON";
-    }
+export const getRouteOwner = (
+  pathname: string,
+): 'ADMIN' | 'USER' | 'COMMON' | null => {
+  if (isRouteMatches(pathname, adminProtectedRoutes)) {
+    return 'ADMIN';
+  }
 
-    return null; // public route
-}
+  if (isRouteMatches(pathname, userProtectedRoutes)) {
+    return 'USER';
+  }
 
-export const getDefaultDashboardRoute = (role : UserRole) => {
-    if(role === "ADMIN" || role === "SUPER_ADMIN") {
-        return "/admin/dashboard";
-    }
-    if(role === "DOCTOR") {
-        return "/doctor/dashboard";
-    }
-    if(role === "PATIENT") {
-        return "/dashboard";
-    }
+  if (isRouteMatches(pathname, commonProtectedRoutes)) {
+    return 'COMMON';
+  }
 
-    return "/";
-}
+  return null;
+};
 
-export const isValidRedirectForRole = (redirectPath : string, role : UserRole) => {
-    const unifySuperAdminAndAdminRole = role === "SUPER_ADMIN" ? "ADMIN" : role;
+export const getDefaultDashboardRoute = (role: UserRole) => {
+  if (role === 'ADMIN') {
+    return '/admin/dashboard';
+  }
 
-    role = unifySuperAdminAndAdminRole;
+  if (role === 'USER') {
+    return '/dashboard';
+  }
 
-    const routeOwner = getRouteOwner(redirectPath);
+  return '/';
+};
 
-    if(routeOwner === null || routeOwner === "COMMON"){
-        return true;
-    }
+export const isValidRedirectForRole = (
+  redirectPath: string,
+  role: UserRole,
+) => {
+  const routeOwner = getRouteOwner(redirectPath);
 
-    if(routeOwner === role){
-        return true;
-    }
+  if (routeOwner === null || routeOwner === 'COMMON') {
+    return true;
+  }
 
-    return false;
-}
+  if (routeOwner === role) {
+    return true;
+  }
+
+  return false;
+};
--- src/zod/auth.validation.ts
+++ src/zod/auth.validation.ts
@@ -5,10 +5,10 @@ export const loginZodSchema = z.object({
     .string()
     .min(6, 'Password must be at least 6 characters')
     .max(100, 'Password must be at most 100 characters')
-    .regex(
-      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,}$/,
-      'Password must contain at least one letter and one number',
-    ),
+    // .regex(
+    //   /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,}$/,
+    //   'Password must contain at least one letter and one number',
+    // ),
 });
 
 export type ILoginPayload = z.infer<typeof loginZodSchema>;
